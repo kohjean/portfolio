ba8a9c1dece0ae5ec5e41d2adccd4a1d8f6bd2e6
ishii <reservist088@gmail.com>
2020-06-13 03:02:55 +0900

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'src');
+const SCSS_DIR = path.join(SRC_DIR, 'assets', 'scss');
+
+const SASS_AUTO_IMPORTS = `
+@import "${path.join(SCSS_DIR, '_variables.scss')}";
+`;
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  css: {
+    loaderOptions: {
+      sass: {
+        data: SASS_AUTO_IMPORTS,
+        includePaths: ['./node_modules'],
+      },
+    },
+  },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <header>
+      <h1 class="text-center" id="what">Portfolio</h1>
+      <h2 class="text-center" id="who">Koji Ishii</h2>
+      <p id="when">
+        <span class="text-wrap">Latest Update</span>
+        <span class="text-wrap">: 2020/ 6/ 11</span>
+      </p>
+    </header>
+    <nav id="content-nav">
+      <ul class="flex text-center">
+        <li @click="changeContent('carrer')">
+          <i class="material-icons">assignment_ind</i>
+          <p>Carrer / Skill</p>
+        </li>
+        <li @click="changeContent('work')">
+          <i class="material-icons">desktop_windows</i>
+          <p>Works</p>
+        </li>
+        <li @click="changeContent('contact')">
+          <i class="material-icons">question_answer</i>
+          <p>Contact / Recruit</p>
+        </li>
+      </ul>
+    </nav>
+    <div id="click-suggest">
+      <ul class="flex text-center"> 
+        <li>
+          <i class="material-icons">keyboard_arrow_up</i>
+        </li>
+        <li>
+          <i class="material-icons">keyboard_arrow_up</i>
+        </li>
+        <li>
+          <i class="material-icons">keyboard_arrow_up</i>
+        </li>
+      </ul>
+      <p class="text-center">Please click icon.</p>
+    </div>
+    <section id="main-contents">
+      <component :is="currentContent">
+        
+      </component>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ContentCarrer from './components/ContentCarrer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContentCarrer
   }
 }
 </script>
 
-<style>
+<style lang="scss" scope>
+@import 'https://fonts.googleapis.com/icon?family=Material+Icons';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
